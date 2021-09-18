71eadf41be2663a92dd31ba02d880fc5b9eb4e66
luke <luke44@gmail.com>
2021-09-18 15:51:12 +0100

    As before, but with rspec tests included

--- spec/sourcecode_spec.rb
+++ spec/sourcecode_spec.rb
@@ -1,5 +1,11 @@
 require './lib/sourcecode.rb'
 
+describe 'the function select_noun' do
+  it 'should return a string' do
+    expect(select_noun()).to be_kind_of(String)
+  end
+end
+
 =begin
 Can't get this test to work, 
 checking that the function exists in the first place
@@ -10,15 +16,21 @@ describe 'check_gender' do
 end
 =end 
 
-describe 'check_gender' do
-  it 'returns "Well done!" when passed m' do
-    expect(check_gender("m")).to eq 'Well done!'
-  end
-end
+# describe 'check_gender' do
+#   it 'returns "Well done!" when passed m' do
+#     expect(check_gender("m")).to eq 'Well done!'
+#   end
+# end
 
-describe 'suggest_noun' do
-  it 'returns a string when passed y' do
-    expect(suggest_noun("y")).to be_kind_of(String)
-  end
-end
+# describe 'suggest_noun' do
+#   it 'returns a string when passed y' do
+#     expect(suggest_noun("y")).to be_kind_of(String)
+#   end
+# end
+
+# describe 'suggest_noun' do
+#   it 'returns "Bye" when passed n' do
+#     expect(suggest_noun("n")).to eq 'Bye!'
+#   end
+# end
 
